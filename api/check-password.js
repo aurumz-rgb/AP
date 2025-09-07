d30920a43ed2eb9441b0637783b1baa6dbbde3f1
aurumz-rgb <vihaansahu143@gmail.com>
2025-09-08 03:22:45 +0530

    Final v2.0.0 commit

--- api/check-password.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { password } = req.body || {};
-  if(password === process.env.DASHBOARD_PASSWORD){
-    res.status(200).json({ success: true });
-  } else {
-    res.status(200).json({ success: false });
-  }
-}
-
